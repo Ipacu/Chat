2a59183b93ce254bc0e8cb56f74897064c9c782b
pAcU <pacu@mail.ru>
2016-02-05 12:42:13 +0200

    1

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io').listen(http);
+
+    //server = require('http').createServer(app),
+    //io = require('socket.io').listen(server),
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+
 app.set('port', (process.env.PORT || 5000));
 
 
