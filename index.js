086aca7ad57c6ec254f9536e4d77877dc2a51386
pAcU <pacu@mail.ru>
2016-02-05 11:53:24 +0200

    dowload files

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+//http.listen(3000, function(){
+//  console.log('listening on *:3000');
+//});
